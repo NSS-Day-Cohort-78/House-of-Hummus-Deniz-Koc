f3fe169edc8e0ab99527421d79bf9b83be5da64b
Deniz Koc <dkocusa@outlook.com>
2025-08-26 19:27:20 -0500

    Implement Sales  and  FoodTruck

--- scripts/FoodTruck.js
+++ scripts/FoodTruck.js
@@ -1,13 +1,25 @@
 import { Sales } from "./Sales.js"
+import { Entrees } from "./Entrees.js"
+import { Sides } from "./SideDishes.js"
+import { Sales } from "./Sales.js"
 
-export const FoodTruck = () => {
+export const FoodTruck = async() => {
     const salesHTML = Sales()
+    const entreesHTML = await Entrees()
+    const veggiesHTML = await Veggies()
+    const sidesHTML   = await Sides()
+    const salesList   = await Sales()
 
     return `
         <header class="header">
             <img src="./images/hummus.png" class="logo" />
             <h1 class="title">Laura Kathryn's House of Hummus</h1>
         </header>
+        <article class="choices">
+            ${entreesHTML}
+            ${veggiesHTML}
+            ${sidesHTML}
+        </article>
 
         <article>
             <button id="purchase">Purchase Combo</button>
--- scripts/Sales.js
+++ scripts/Sales.js
@@ -1,10 +1,15 @@
+
 export const Sales = async () => {
-    const sales = await fetch("http://localhost:8088/orders").then(res => res.json())
+  const sales = await fetch("http://localhost:8088/purchases?_expand=entree&_expand=vegetable&_expand=side")
+    .then(res => res.json())
 
-    let salesDivs = sales.map()
+  let salesDivs = sales.map((purchase) => {
+    const total = purchase.entree.price + purchase.vegetable.price + purchase.side.price
+    return `<li>Receipt #${purchase.id} = $${total.toFixed(2)}</li>`
+  })
 
-    salesDivs = salesDivs.join("")
+  salesDivs = salesDivs.join("")
 
-    return salesDivs
+  return salesDivs
 }
 
